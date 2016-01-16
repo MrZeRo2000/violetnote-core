2ea01a0a5668e49d88f39942264f0129cd5e3a26
MrZeRo2000 <MrZeRo2000@mail.ru>
2016-01-16 14:20:03 +0200

    first commit

---
+++ src/main/java/com/romanpulov/violetnotecore/model/Model1.java
@@ -0,0 +1,6 @@
+package com.romanpulov.violetnotecore.model;
+
+public class Model1 {
+	public void method1() {
+	}
+}
---
+++ src/test/java/com/romanpulov/violetnotecore/Test1.java
@@ -0,0 +1,12 @@
+package com.romanpulov.violetnotecore;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class Test1 {
+
+    @Test
+    public void method1() {
+        assertEquals(1, 2);
+    }
+}
