81065c01b76c2e271bcfbf6dfc2c61904140d72f
MrZeRo2000 <MrZeRo2000@mail.ru>
2016-10-15 20:46:01 +0300

    added tests for crypting timings

--- src/test/java/com/romanpulov/violetnotecore/TestCryptography.java
+++ src/test/java/com/romanpulov/violetnotecore/TestCryptography.java
@@ -14,6 +14,8 @@ import javax.crypto.spec.SecretKeySpec;
 import java.security.AlgorithmParameters;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.KeySpec;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -47,24 +49,64 @@ public class TestCryptography {
     public void keyGen() throws Exception {
         final int ITERATIONS = 65536;
         final int KEYLEN_SIZE = 128;
+        Map<String, Long> timeline = new HashMap<>();
         try {
+            long prevTime = System.nanoTime();
             byte[] salt = AESCryptService.generateSalt(8);
+            long currentTime = prevTime;
+            timeline.put("GenerateSalt", currentTime - prevTime);
+
             String password = "Password1fg sdfg sdfg sdfg sdfg dsgf sdfg sdsfg ";
-            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+
+            prevTime = System.nanoTime();
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            currentTime = System.nanoTime();
+            timeline.put("Get SecretKeyFactorty instance", currentTime - prevTime);
+
+            prevTime = System.nanoTime();
             KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, ITERATIONS, KEYLEN_SIZE);
+            currentTime = System.nanoTime();
+            timeline.put("Get KeySpec", currentTime - prevTime);
+
+            prevTime = System.nanoTime();
             SecretKey key = factory.generateSecret(spec);
+            currentTime = System.nanoTime();
+            timeline.put("generateSecret", currentTime - prevTime);
+
+            prevTime = System.nanoTime();
             SecretKey secret = new SecretKeySpec(key.getEncoded(), "AES");
+            currentTime = System.nanoTime();
+            timeline.put("get SecretKeySpec", currentTime - prevTime);
 
             /* Encrypt the message. */
+            prevTime = System.nanoTime();
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            currentTime = System.nanoTime();
+            timeline.put("get Cipher instance", currentTime - prevTime);
+
+            prevTime = System.nanoTime();
             cipher.init(Cipher.ENCRYPT_MODE, secret);
+            currentTime = System.nanoTime();
+            timeline.put("init Cipher", currentTime - prevTime);
+
+            prevTime = System.nanoTime();
             AlgorithmParameters params = cipher.getParameters();
             byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
+            currentTime = System.nanoTime();
+            timeline.put("init IV", currentTime - prevTime);
             System.out.println("iv size:" + iv.length);
 
+            prevTime = System.nanoTime();
             byte[] ciphertext = cipher.doFinal("Hello, World!".getBytes("UTF-8"));
             System.out.println(HexConverter.bytesToHex(ciphertext));
+            currentTime = System.nanoTime();
+            timeline.put("crypt message", currentTime - prevTime);
 
+            System.out.println("*** Timeline ***");
+            for (Map.Entry<String, Long> entry : timeline.entrySet()) {
+                System.out.println(entry.getKey() + " - " + entry.getValue()/1000);
+            }
+            System.out.println("***");
 
             /* Decrypt the message, given derived key and initialization vector. */
             cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
@@ -75,6 +117,12 @@ public class TestCryptography {
         } catch (NoSuchAlgorithmException e) {
             throw e;
         }
+    }
+
+    @Test
+    public void multipleKeyGen() throws Exception {
+        for (int i = 0; i < 10; i ++)
+            keyGen();
     }
 
 }
